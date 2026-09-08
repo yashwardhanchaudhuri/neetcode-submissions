e3e326bda7786d6a5048ca274480f9cab8cbb9ba
Yashwardhan Chaudhuri <84658100+yashwardhanchaudhuri@users.noreply.github.com>
2026-09-08 09:55:54 +0530

    Add: contains-duplicate-ii - submission-12

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-12.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+
+        if k < 1:
+            return False
+        hm = defaultdict(int)
+        i = 0
+        hm[nums[i]] += 1
+
+        for j in range(1, len(nums)):
+            hm[nums[j]] += 1
+
+            if abs(i - j) > k:
+                hm[nums[i]] -= 1
+                i += 1
+
+            if hm[nums[j]] == 2:
+                return True
+
+        print(hm)
+        return False     
